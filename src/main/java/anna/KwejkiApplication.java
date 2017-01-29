f2569f0cd05b5c0135c44eebe18d6ed5458b44f9
Anna Zglejszewska <annazglejszewska@wp.pl>
2017-01-29 11:05:46 +0100

    first commit

---
+++ src/main/java/anna/KwejkiApplication.java
@@ -0,0 +1,12 @@
+package anna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KwejkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KwejkiApplication.class, args);
+	}
+}
