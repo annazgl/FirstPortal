5b869c4c7ca8e2d65784bcc7c4202776f04f2cd6
Anna Zglejszewska <annazglejszewska@wp.pl>
2017-01-29 12:10:52 +0100

    added gif repository

--- src/main/java/anna/controller/GifController.java
+++ src/main/java/anna/controller/GifController.java
@@ -1,6 +1,8 @@
 package anna.controller;
 
 import anna.model.Gif;
+import anna.repository.GifRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class GifController {
 
+    @Autowired
+    private GifRepository gifRepository;
+
     @GetMapping("/")
     public String listsGifs(){
         return "home";
@@ -16,7 +21,7 @@ public class GifController {
 
     @GetMapping("/gif")
     public String gifDetails(ModelMap modelMap){
-        Gif gif= new Gif("android-explosion", "anna",true);
+        Gif gif= gifRepository.findByName("android-explosion");
         modelMap.put("gif",gif);
         return "gif-details";
     }
---
+++ src/main/java/anna/repository/GifRepository.java
@@ -0,0 +1,28 @@
+package anna.repository;
+
+import anna.model.Gif;
+import org.springframework.stereotype.Repository;
+import sun.net.www.content.image.gif;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Repository
+public class GifRepository {
+    private static final List<Gif>ALL_GIFS = Arrays.asList(
+        new Gif("android-explosion","anna", false),
+            new Gif("ben-and-mike",  "anna", true),
+            new Gif("book-dominos", "anna", false),
+            new Gif("compiler-bot", "anna", true),
+            new Gif("cowboy-coder",  "anna", false),
+            new Gif("infinite-andrew", "anna", true)
+    );
+
+    public Gif findByName(String name){
+        for(Gif gif: ALL_GIFS){
+            if(gif.getName().equals(name))
+                return gif;
+        }
+        return null;
+    }
+}
