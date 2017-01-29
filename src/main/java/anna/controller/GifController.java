d0748e980c69a6cb1163b57fa6d575006ff7cd73
Anna Zglejszewska <annazglejszewska@wp.pl>
2017-01-29 11:31:47 +0100

    added gifs details

--- src/main/java/anna/controller/GifController.java
+++ src/main/java/anna/controller/GifController.java
@@ -1,6 +1,8 @@
 package anna.controller;
 
+import anna.model.Gif;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -10,5 +12,12 @@ public class GifController {
     @GetMapping("/")
     public String listsGifs(){
         return "home";
+    }
+
+    @GetMapping("/gif")
+    public String gifDetails(ModelMap modelMap){
+        Gif gif= new Gif("android-explosion", "anna",true);
+        modelMap.put("gif",gif);
+        return "gif-details";
     }
 }
