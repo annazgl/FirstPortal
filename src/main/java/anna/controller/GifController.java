375e3eb142dec48bf2f1148bdb97b593b17858a0
Anna Zglejszewska <annazglejszewska@wp.pl>
2017-01-29 12:35:17 +0100

    added all gifs

--- src/main/java/anna/controller/GifController.java
+++ src/main/java/anna/controller/GifController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class GifController {
@@ -15,9 +18,11 @@ public class GifController {
     private GifRepository gifRepository;
 
     @GetMapping("/")
-    public String listsGifs(){
+    public String listGifs(ModelMap modelMap){
+        modelMap.addAttribute("gifs",gifRepository.getAllGifs());
         return "home";
     }
+
 
     @GetMapping("/gif")
     public String gifDetails(ModelMap modelMap){
