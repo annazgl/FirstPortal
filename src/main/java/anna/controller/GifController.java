e7232405c4695160284a746bc99f1c457cc7a050
Anna Zglejszewska <annazglejszewska@wp.pl>
2017-02-25 09:56:56 +0100

    removed a image

--- src/main/java/anna/controller/GifController.java
+++ src/main/java/anna/controller/GifController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
-import sun.net.www.content.image.gif;
+
 
 import java.util.ArrayList;
 import java.util.List;
